abf10b567fc330d991d694f9e4bc55d57b7db3a8
Aadi Designer <aadityasuryawanshi44@gmail.com>
2024-07-30 14:40:38 +0530

    Add files via upload

---
+++ jsDay18/CharacterOccurrences.js
@@ -0,0 +1,8 @@
+
+function countCharacterOccurrences(str) {
+    let counts = {};
+    for (let char of str) {
+        counts[char] = (counts[char] || 0) + 1;
+    }
+    return counts;
+}
---
+++ jsDay18/Quicksort.js
@@ -0,0 +1,11 @@
+
+function quicksort(arr) {
+    if (arr.length <= 1) return arr;
+    let pivot = arr[arr.length - 1];
+    let left = [], right = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) left.push(arr[i]);
+        else right.push(arr[i]);
+    }
+    return [...quicksort(left), pivot, ...quicksort(right)];
+}
