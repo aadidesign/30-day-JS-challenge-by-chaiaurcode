0ddc6773cf217c63fb0b1afa4bb4038117749250
Aadi Designer <aadityasuryawanshi44@gmail.com>
2024-08-03 23:31:04 +0530

    Add files via upload

---
+++ jsDay22/task3_container_with_most_water.js
@@ -0,0 +1,12 @@
+// Container With Most Water
+function maxArea(height) {
+  let maxarea = 0;
+  let l = 0;
+  let r = height.length - 1;
+  while (l < r) {
+    maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+    if (height[l] < height[r]) l++;
+    else r--;
+  }
+  return maxarea;
+}
