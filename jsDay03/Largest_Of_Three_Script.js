95f4b95eab724c40af6e2f813c90f6707a45d5eb
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-02 19:48:33 +0530

    Update Largest_Of_Three_Script.js

--- jsDay03/Largest_Of_Three_Script.js
+++ jsDay03/Largest_Of_Three_Script.js
@@ -1,13 +1,14 @@
 
 // Task 3: Write a program to find the largest of three numbers using nested if-else statements.
-function findLargest(a, b, c) {
-  if (a >= b && a >= c) {
-    console.log("Largest:", a);
-  } else if (b >= a && b >= c) {
-    console.log("Largest:", b);
-  } else {
-    console.log("Largest:", c);
-  }
+function findLargest(a, b, c){
+    if (a>=b && a>=c){
+        console.log("Largest", a);
+    } else if (b>=a && b>= c) {
+        console.log("Largest", b);
+    } else {
+        console.log("Largest", c);
+    }
 }
-findLargest(1, 5, 3);
-findLargest(10, 7, 9);
+
+findLargest(1,3,5);
+findLargest(8, 15, 2)
