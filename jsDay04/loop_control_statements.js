18ac03ec98e2bf44bee094a429478a79d2040f5e
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-02 20:29:35 +0530

    Update loop_control_statements.js

--- jsDay04/loop_control_statements.js
+++ jsDay04/loop_control_statements.js
@@ -8,8 +8,8 @@ for (let i = 1; i <= 10; i++) {
 }
 
 // Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
-for (let i = 1; i <= 10; i++) {
-    if (i === 7) {
+for (let i=1; i<=10; i++){
+    if (i===7) {
         break;
     }
     console.log(i);
