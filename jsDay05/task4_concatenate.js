b0c7014442bc4cb6678a866fbcc80aa18b020a8f
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-03 11:04:17 +0530

    Update task4_concatenate.js

--- jsDay05/task4_concatenate.js
+++ jsDay05/task4_concatenate.js
@@ -1,4 +1,12 @@
-
 const concatenateStrings = function(str1, str2) {
-    return str1 + str2;
+    const string = str1+str2;
+    console.log('Concatenated string is:', string)
 }
+
+let i1= prompt('Enter first string:');
+let str1= String(i1);
+
+let i2= prompt('Enter second string:');
+let str2= String(i2);
+
+concatenateStrings(str1, str2);
