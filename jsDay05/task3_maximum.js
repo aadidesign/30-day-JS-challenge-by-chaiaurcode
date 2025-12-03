09fa31dc353422d539ee4e9f590f4ad8edb2dcb7
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-03 10:47:15 +0530

    Update task3_maximum.js

--- jsDay05/task3_maximum.js
+++ jsDay05/task3_maximum.js
@@ -1,5 +1,12 @@
-
-const findMaximum = function(a, b) {
+const findMaximum = function (a, b) {
     const max = (a > b) ? a : b;
     console.log('Maximum is:', max);
 }
+
+let input1 = prompt("Enter the first number:");
+let num1 = Number(input1);
+
+let input2 = prompt("Enter the second number:");
+let num2 = Number(input2);
+
+findMaximum(num1, num2);
