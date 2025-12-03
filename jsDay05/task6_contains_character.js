d716fd0426fea5d55cc6439bec08f8aa11887089
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-03 22:04:31 +0530

    Update task6_contains_character.js

--- jsDay05/task6_contains_character.js
+++ jsDay05/task6_contains_character.js
@@ -1,2 +1,7 @@
-
+// Function Definition
 const containsCharacter = (str, char) => str.includes(char);
+
+// How it works in practice:
+console.log(containsCharacter("Hello World", "World")); // true
+console.log(containsCharacter("Hello World", "z"));     // false
+console.log(containsCharacter("Hello World", "h"));     // false (Case sensitive!)
