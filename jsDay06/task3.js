7aad4bdf82da5dbca6de973751753efdcd946471
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-03 22:40:38 +0530

    Update task3.js

--- jsDay06/task3.js
+++ jsDay06/task3.js
@@ -1,12 +1,16 @@
+let arr1 = [0,1, 2, 3, 4]
+console.log(arr1);
+// console.log(arr1[0]);
+// console.log(arr1[arr1.length-1]);
 
-// Task 7: Use the map method to create a new array where each number is doubled and log the new array.
-let doubledArr = arr1.map(num => num * 2);
+// map method to create a new array where each number is doubled and log the new array.
+let doubledArr = arr1.map(num => num*2);
 console.log(doubledArr);
 
-// Task 8: Use the filter method to create a new array with only even numbers and log the new array.
+// filter method to create a new array with only even numbers and log the new array.
 let evenArr = arr1.filter(num => num % 2 === 0);
 console.log(evenArr);
 
-// Task 9: Use the reduce method to calculate the sum of all numbers in the array and log the result.
-let sum = arr1.reduce((total, num) => total + num, 0);
+// reduce method to calculate the sum of all numbers in the array and log the result.
+let sum = arr1.reduce((total, num) => total+num, 0);
 console.log(sum);
