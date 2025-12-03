1c6dd69978e73700ea72741315467df9f3851b2c
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-03 22:09:19 +0530

    Update task7_product.js

--- jsDay05/task7_product.js
+++ jsDay05/task7_product.js
@@ -1,4 +1,10 @@
-
-function multiply(a, b = 1) {
+function multiply(a, b = 1) { // If 'b' is missing, it becomes 1
     return a * b;
 }
+
+// Scenario A: Both arguments provided
+console.log(multiply(5, 2)); // Output: 10
+
+// Scenario B: Second argument missing
+console.log(multiply(5));    // Output: 5
+// Logic: 5 * 1 = 5. (Without the default, this would be 5 * undefined = NaN)
