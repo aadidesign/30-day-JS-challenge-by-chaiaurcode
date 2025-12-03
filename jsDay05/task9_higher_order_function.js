e49b63a9627b039b6570dd9814c5ae71d7562a34
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-03 22:21:24 +0530

    Update task9_higher_order_function.js

--- jsDay05/task9_higher_order_function.js
+++ jsDay05/task9_higher_order_function.js
@@ -1,6 +1,13 @@
-
+// 1. The HOF 
 function higherOrderFunction(fn, num) {
     for (let i = 0; i < num; i++) {
-        fn();
+        fn(); // Execute the passed function
     }
 }
+
+// 2. The Callback (The instructions we want to repeat)
+const sayHello = () => console.log("Hello!");
+
+// 3. Execution
+// Notice we pass 'sayHello', NOT 'sayHello()'
+higherOrderFunction(sayHello, 3);
