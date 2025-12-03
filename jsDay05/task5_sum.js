151d28a76a435696bb8338f8aef25145a2d61879
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-03 11:08:32 +0530

    Update task5_sum.js

--- jsDay05/task5_sum.js
+++ jsDay05/task5_sum.js
@@ -1,2 +1,9 @@
-
 const sum = (a, b) => a + b;
+
+let i1 = prompt('Enter 1st number:');
+let a = Number(i1);
+
+let i2 = prompt('Enter 2nd number:');
+let b = Number(i2);
+
+console.log('Sum of 1st and 2nd number is :', sum(a,b))
