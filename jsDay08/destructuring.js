ff46a6ffd5952bb6c71907eceda8dcbe5386af48
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-04 16:44:58 +0530

    Update destructuring.js

--- jsDay08/destructuring.js
+++ jsDay08/destructuring.js
@@ -4,6 +4,9 @@
 const numbers = [1, 2, 3, 4, 5];
 const [first, second] = numbers;
 console.log(first, second);
+// const [first, second, third] = numbers;
+// console.log(first, second, third);
+
 
 // Task 4
 const book = { title: "1984", author: "George Orwell" };
