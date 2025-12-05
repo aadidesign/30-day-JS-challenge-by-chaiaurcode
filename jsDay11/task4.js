262635ec4e3e2a625bc88bedc7a2b42443c39c41
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-05 18:49:24 +0530

    Update task4.js

--- jsDay11/task4.js
+++ jsDay11/task4.js
@@ -1,4 +1,4 @@
-async function asyncFunction1() {
+async function asyncFunction1(){
     const promise = new Promise((resolve) => {
         setTimeout(() => resolve('Resolved promise'), 2000);
     });
