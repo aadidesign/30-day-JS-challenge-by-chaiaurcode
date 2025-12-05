a20c80e75f085cee52743ee3985463328c9f27bd
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-05 20:05:22 +0530

    Update task8.js

--- jsDay11/task8.js
+++ jsDay11/task8.js
@@ -1,6 +1,6 @@
-const promiseA = new Promise((resolve) => setTimeout(() => resolve('Promise A resolved'), 1000));
-const promiseB = new Promise((resolve) => setTimeout(() => resolve('Promise B resolved'), 2000));
-const promiseC = new Promise((resolve) => setTimeout(() => resolve('Promise C resolved'), 3000));
+const promiseA = new Promise((resolve) => setTimeout(() => resolve('Promise A is resolved'), 1000));
+const promiseB = new Promise((resolve) => setTimeout(() => resolve('Promise B is resolved'), 2000));
+const promiseC = new Promise((resolve) => setTimeout(() => resolve('Promise C is resolved'), 3000));
 
 Promise.all([promiseA, promiseB, promiseC])
-    .then(values => console.log(values));
+.then(values => console.log(values));
