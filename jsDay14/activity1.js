44b4309f4a17de76b3c8d81d81c39606d16d7a21
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-06 17:11:13 +0530

    Update activity1.js

--- jsDay14/activity1.js
+++ jsDay14/activity1.js
@@ -1,22 +1,21 @@
-
 // Task 1
 class Person {
-    constructor(name, age) {
+    constructor(name, age){
         this.name = name;
         this.age = age;
     }
-
-    greeting() {
-        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    
+    greeting(){
+        return `Hello! my name is ${this.name} and I am ${this.age} years old.`;
     }
 }
 
 // Task 2
-Person.prototype.updateAge = function(newAge) {
+Person.prototype.updateAge= function(newAge){
     this.age = newAge;
-    console.log(`Updated age: ${this.age}`);
+    console.log(`Updated Age: ${this.age}`);
 };
 
 const person1 = new Person('John', 25);
 console.log(person1.greeting());
-person1.updateAge(26);
+person1.updateAge(29);
