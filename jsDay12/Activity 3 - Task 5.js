9e05b23eb6378fa56aa8f97c94d491982bd61a96
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-12-06 14:51:26 +0530

    Update Activity 3 - Task 5.js

--- jsDay12/Activity 3 - Task 5.js
+++ jsDay12/Activity 3 - Task 5.js
@@ -1,4 +1,3 @@
-
 // Task 5: Write a function that validates user input (e.g., checking if a string is not empty) and throws a custom error if the validation fails. Handle the custom error using a try-catch block.
 class ValidationError extends Error {
     constructor(message) {
@@ -6,6 +5,8 @@ class ValidationError extends Error {
         this.name = "ValidationError";
     }
 }
+
+
 
 function validateInput(input) {
     try {
